0e4c7737035abfe753994813ab69d992b05c37dc
seonguk_kang <dnr2144@gmail.com>
2025-01-10 14:07:42 +0900

    :chore: fix change github folder
    :chore: add github action activation branch
    
    :chore: add github action activation branch2
    
    :chore: add github action activation test3
    
    :chore: add github action activation test4

---
+++ src/test/java/com/threemeals/delivery/domain/menu/service/MenuServiceTest.java
@@ -0,0 +1,137 @@
+package com.threemeals.delivery.domain.menu.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.util.ReflectionTestUtils.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.threemeals.delivery.domain.menu.dto.request.MenuRequestDto;
+import com.threemeals.delivery.domain.menu.dto.response.MenuResponseDto;
+import com.threemeals.delivery.domain.menu.entity.Category;
+import com.threemeals.delivery.domain.menu.entity.Menu;
+import com.threemeals.delivery.domain.menu.repository.MenuRepository;
+import com.threemeals.delivery.domain.store.entity.Store;
+import com.threemeals.delivery.domain.store.repository.StoreRepository;
+import com.threemeals.delivery.domain.user.entity.User;
+import com.threemeals.delivery.domain.user.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+class MenuServiceTest {
+
+	@Mock
+	MenuRepository menuRepository;
+
+	@Mock
+	UserService userService;
+
+	@Mock
+	StoreRepository storeRepository;
+
+	@InjectMocks
+	MenuService menuService;
+
+	@Test
+	void 메뉴를_성공적으로_추가() {
+		// given
+		MenuRequestDto requestDto = makeMenuRequestDto();
+		User mockOwner = makeMockOwner();
+		Store mockStore = makeMockStore(mockOwner);
+		Menu mockMenu = makeMockMenu(requestDto);
+		MenuResponseDto expectedResponse = MenuResponseDto.fromEntity(mockMenu);
+
+		when(userService.getOwnerById(anyLong())).thenReturn(mockOwner);
+		when(storeRepository.findById(anyLong())).thenReturn(Optional.of(mockStore));
+		when(menuRepository.save(any(Menu.class))).thenReturn(mockMenu);
+
+		// when
+		MenuResponseDto actualResponse = menuService.addMenu(mockStore.getId(), mockOwner.getId(), requestDto);
+
+		// then
+		assertThat(actualResponse)
+			.usingRecursiveComparison()
+			.isEqualTo(expectedResponse);
+	}
+
+	@Test
+	void 메뉴_업데이트_성공() {
+		// given
+		MenuRequestDto requestDto = makeMenuRequestDto();
+		User mockOwner = makeMockOwner();
+		Menu mockMenu = makeMockMenu(requestDto);
+		MenuResponseDto expectedResponse = MenuResponseDto.fromEntity(mockMenu);
+
+		when(menuRepository.findById(anyLong())).thenReturn(Optional.of(mockMenu));
+		when(menuRepository.existsByMenuIdAndOwnerId(anyLong(), anyLong())).thenReturn(true);
+
+		// when
+		MenuResponseDto actualResponse = menuService.updateMenu(mockOwner.getId(), mockMenu.getId(), requestDto);
+
+		// then
+		assertThat(actualResponse)
+			.usingRecursiveComparison()
+			.isEqualTo(expectedResponse);
+	}
+
+	@Test
+	void 메뉴_삭제_성공() {
+		// given
+		MenuRequestDto requestDto = makeMenuRequestDto();
+		Menu mockMenu = makeMockMenu(requestDto);
+		User mockOwner = makeMockOwner();
+
+		when(menuRepository.findById(anyLong())).thenReturn(Optional.of(mockMenu));
+		when(menuRepository.existsByMenuIdAndOwnerId(anyLong(), anyLong())).thenReturn(true);
+
+		// when
+		menuService.deleteMenu(mockMenu.getId(), mockOwner.getId());
+
+		// then
+		assertThat(mockMenu.getIsDeleted()).isTrue();
+	}
+
+	private Menu makeMockMenu(MenuRequestDto requestDto) {
+		Menu mockMenu = Menu.builder()
+			.menuName(requestDto.menuName())
+			.category(Category.of(requestDto.category()))
+			.menuImgUrl(requestDto.menuImgUrl())
+			.price(requestDto.price())
+			.description(requestDto.description())
+			.build();
+		setField(mockMenu, "id", 1L);
+		return mockMenu;
+	}
+
+	private Store makeMockStore(User mockOwner) {
+		Store mockStore = Store.builder().build();
+		setField(mockStore, "id", 1L);
+		setField(mockStore, "owner", mockOwner);
+		return mockStore;
+	}
+
+	private User makeMockOwner() {
+		User mockOwner = User.builder().build();
+		setField(mockOwner, "id", 1L);
+		return mockOwner;
+	}
+
+	private MenuRequestDto makeMenuRequestDto() {
+		MenuRequestDto requestDto = new MenuRequestDto(
+			"KOREAN",
+			"불닭좋아",
+			"삼양 짱짱",
+			12000,
+			"https:~~~"
+		);
+		return requestDto;
+	}
+}
